1cb6b162e5ee66bf39f075962038c453a8fa5fe8
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 20:19:24 +0100

    Update input style

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Icon from "../icons/Icon";
 import s from "./searchbar.module.css";
 
 const Searchbar = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <div id={s.searchbar}>
       <Icon name="magnify" />
-      <input type="text" style={{ flexGrow: 1 }} />
+      <div className={s.searchInputWrapper}>
+        <input
+          value={search}
+          name="search"
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          className={s.searchInput}
+        />
+        <span className={s.focusIndicator}></span>
+      </div>
+
       <button className={s.searchbarBtn}>
         <Icon name="plus" color="#fff" />
         Create recipe
