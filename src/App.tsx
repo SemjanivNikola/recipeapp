b061bc5ab37cd6727aec214c32eaf99bb1f0e4a2
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 12:56:15 +0100

    Include dashboard to app entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
+import Dashboard from "./features/dashboard/Dashboard";
 import Drawer from "./features/drawer/Drawer";
 
 function App() {
   return (
     <main id="main">
       <Drawer />
-      <div>
-        <h1>Recipe App</h1>
-      </div>
+      <Dashboard />
     </main>
   );
 }
