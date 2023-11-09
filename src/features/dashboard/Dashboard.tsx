462183af74b2914fc277ab6235caf4bfbcd3c50a
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 12:55:46 +0100

    Create dashboard screen

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import ScreenHeader from "@/components/scren-header/ScreenHeader";
+import s from "./dashboard.module.css";
+
+const Dashboard = () => {
+  return (
+    <div className={s.screen}>
+      <ScreenHeader title="Dashboard" />
+      <div className="content">
+        <div className="search-bar"></div>
+        <table></table>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
