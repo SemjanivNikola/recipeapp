bc7765aa31f07400a288d4deca5bc838dabfbce4
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 15:58:35 +0100

    Add searchbar to dashboard screen

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
 import ScreenHeader from "@/components/screen-header/ScreenHeader";
 import s from "./dashboard.module.css";
+import Searchbar from "@/components/searchbar/Searchbar";
 
 const Dashboard = () => {
   return (
     <div className={s.screen}>
       <ScreenHeader title="Dashboard" />
       <div className="content">
-        <div className="search-bar"></div>
+        <Searchbar />
         <table></table>
       </div>
     </div>
