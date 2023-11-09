9e48d7a5e1dc4b3fc23a6e65563d0c550fb82875
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 10:25:47 +0100

    Add icon name type

---
+++ src/components/icons/IconNameType.ts
@@ -0,0 +1,11 @@
+type IconName =
+  | "book-open-page-variant"
+  | "chevron-left"
+  | "chevron-right"
+  | "dashboard"
+  | "layers-plus"
+  | "layers"
+  | "plus"
+  | "signout";
+
+export default IconName;
