30a45a67faa3de7464e8c206ef282bd6e275f209
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-09 20:47:40 +0100

    Add router provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./features/dashboard/Dashboard.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
