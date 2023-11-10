ac80ffd9dbf6c8b984f980215944203e19bf8a64
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-10 10:37:37 +0100

    Rename Dashboard screen

--- src/features/dashboard/DashboardScreen.tsx
+++ src/features/dashboard/DashboardScreen.tsx
@@ -1,22 +1,24 @@
 import ScreenHeader from "@/components/screen-header/ScreenHeader";
-import s from "./dashboard.module.css";
 import Searchbar from "@/components/searchbar/Searchbar";
+import { Link } from "react-router-dom";
 import RecipeList from "./RecipeList";
+import s from "./dashboard.module.css";
 
 const recipeList = [
   { id: "1", title: "Grilled beef steak and asparagus", dateCreated: "2023-01-01", tags: ["stake", "grill"] },
 ];
 
-const Dashboard = () => {
+const DashboardScreen = () => {
   return (
-    <div className={s.screen}>
+    <>
       <ScreenHeader title="Dashboard" />
       <div className={s.content}>
         <Searchbar />
+        <Link to="recipe/123">Details</Link>
         <RecipeList list={recipeList} />
       </div>
-    </div>
+    </>
   );
 };
 
-export default Dashboard;
+export default DashboardScreen;
