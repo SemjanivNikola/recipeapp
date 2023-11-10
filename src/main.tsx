65617e609020f5f36289447b4189b40ee62c81e2
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-10 10:38:18 +0100

    Switch app to use react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Dashboard from "./features/dashboard/Dashboard.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes/router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-    <App />
   </React.StrictMode>
 );
