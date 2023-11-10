c55e515899a28b6ac54f836490051d4a38c15269
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-10 20:04:37 +0100

    Add useForm hook to screen

--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -2,21 +2,32 @@ import TextInput from "@/components/text-input/TextInput";
 import { useState } from "react";
 import s from "./auth.module.css";
 import { Link } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onSubmit: SubmitHandler<LoginForm> = (data) => console.log(data);
+
   return (
     <div id={s.authScreen}>
       <div className={s.loginContainer}>
         <h1 className={s.formTitle}>Welcome back!</h1>
-        <form action="/login" className={s.authForm}>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.authForm}>
           <TextInput type="email" label="Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <TextInput
+            {...register("password", {
+              required: true,
+            })}
             type="pasword"
             label="Password"
-            name="password"
             value={pass}
             onChange={(e) => setPass(e.target.value)}
           />
