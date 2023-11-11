3b0dd12f988b53f13c6bb450847e21725c5729ac
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-11 20:28:49 +0100

    Add hoc and forward ref to LoginForm component

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,38 +1,40 @@
 import TextInput from "@/components/text-input/TextInput";
-import { Control, Controller, FormState } from "react-hook-form";
-import { LoginForm as FormType } from "./AuthFormType";
+import { forwardRef } from "react";
+import { Controller } from "react-hook-form";
+import withFormController from "./withFormController";
+import { LoginFormProps } from "./AuthFormType";
 
-const LoginForm = ({ control, errors }: { control: Control; errors: FormState<FormType>["errors"] }) => {
-  return (
-    <>
-      <Controller
-        name="email"
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({ field: props }) => (
-          <TextInput type="email" label="Email" autoComplete="email" error={errors?.email?.message} {...props} />
-        )}
-      />
+export const LoginForm = withFormController<LoginFormProps>(
+  forwardRef<HTMLDivElement | null, LoginFormProps>(({ control, errors }, ref) => {
+    return (
+      <div ref={ref}>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: props }) => (
+            <TextInput type="email" label="Email" autoComplete="email" error={errors?.email?.message} {...props} />
+          )}
+        />
 
-      <Controller
-        name="password"
-        control={control}
-        rules={{
-          required: true,
-          pattern: {
-            value:
-              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[?!@#$%^&*()-_+=.,~[\]{};:'"|/><()])[A-Za-z\d?!@#$%^&*()-_+=.,~[\]{};:'"|/><()]{4,}$/,
-            message: "Ne valja",
-          },
-        }}
-        render={({ field: props }) => (
-          <TextInput type="password" label="Password" error={errors?.password?.message} {...props} />
-        )}
-      />
-    </>
-  );
-};
-
-export default LoginForm;
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: true,
+            pattern: {
+              value:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[?!@#$%^&*()-_+=.,~[\]{};:'"|/><()])[A-Za-z\d?!@#$%^&*()-_+=.,~[\]{};:'"|/><()]{4,}$/,
+              message: "Ne valja",
+            },
+          }}
+          render={({ field: props }) => (
+            <TextInput type="password" label="Password" error={errors?.password?.message} {...props} />
+          )}
+        />
+      </div>
+    );
+  })
+);
