0e41476eeb20d1f0f350c820f0ae9f0991eb6161
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-12 12:06:37 +0100

    Use AuthLayout in auth screens

--- src/features/auth/LoginScreen.tsx
+++ src/features/auth/LoginScreen.tsx
@@ -1,25 +1,19 @@
 import { Link } from "react-router-dom";
+import AuthLayout from "./AuthLayout";
 import LoginForm from "./LoginForm";
-import s from "./auth.module.css";
 
 const LoginScreen = () => {
   return (
-    <div id={s.authScreen}>
-      <div className={s.formContainer}>
-        <h1 className={s.formTitle}>Welcome back!</h1>
-        <div style={{ width: "100%" }}>
-          <LoginForm />
-          <span className={s.divider}></span>
-          <p className={s.text}>
-            Don't have an account?
-            <Link to="/register">
-              {" "}
-              <u>Sign up</u>
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    <AuthLayout
+      title="Welcome back!"
+      footerText="Don't have an account?"
+      link={
+        <Link to="/register">
+          <u>Sign up</u>
+        </Link>
+      }
+      render={<LoginForm />}
+    />
   );
 };
 
--- src/features/auth/RegisterScreen.tsx
+++ src/features/auth/RegisterScreen.tsx
@@ -1,25 +1,19 @@
 import { Link } from "react-router-dom";
 import RegisterForm from "./RegisterForm";
-import s from "./auth.module.css";
+import AuthLayout from "./AuthLayout";
 
 const RegisterScreen = () => {
   return (
-    <div id={s.authScreen}>
-      <div className={s.formContainer}>
-        <h1 className={s.formTitle}>Lets go!</h1>
-        <div style={{ width: "100%" }}>
-          <RegisterForm />
-          <span className={s.divider}></span>
-          <p className={s.text}>
-            Already cooking with RecipeApp?
-            <Link to="/login">
-              {" "}
-              <u>Sign in</u>
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
+    <AuthLayout
+      title="Lets go!"
+      footerText="Already cooking with RecipeApp?"
+      link={
+        <Link to="/login">
+          <u>Sign in</u>
+        </Link>
+      }
+      render={<RegisterForm />}
+    />
   );
 };
 
