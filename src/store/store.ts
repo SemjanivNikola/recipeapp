e85320906b559db57f7e2dbbbff55953475d8b17
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-12 19:43:25 +0100

    Add reducers + middleware config to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api/api";
+import auth from "@/features/auth/slices/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
