b6bbe94e63262cf26b20388a248ab2fd7fd807b4
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-12 22:18:48 +0100

    Update matcher methods

--- src/features/auth/slices/authSlice.ts
+++ src/features/auth/slices/authSlice.ts
@@ -1,7 +1,5 @@
+import { User, userApi } from "@/store/services/userApi";
 import { createSlice } from "@reduxjs/toolkit";
-// import { postsApi } from "../../app/services/posts";
-import { RootState } from "@/store/store";
-import { User } from "@/store/services/userApi";
 
 interface AuthSlice {
   user: null | User;
@@ -13,28 +11,24 @@ const initialState: AuthSlice = {
   isAuthenticated: false,
 };
 
-const slice = createSlice({
+const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: initialState,
   reducers: {
     logout: () => initialState,
   },
-  //   extraReducers: (builder) => {
-  //     builder
-  //       .addMatcher(postsApi.endpoints.login.matchPending, (_, action) => {
-  //         console.log("pending", action);
-  //       })
-  //       .addMatcher(postsApi.endpoints.login.matchFulfilled, (state, action) => {
-  //         console.log("fulfilled", action);
-  //         state.user = action.payload.user;
-  //       })
-  //       .addMatcher(postsApi.endpoints.login.matchRejected, (_, action) => {
-  //         console.log("rejected", action);
-  //       });
-  //   },
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(userApi.endpoints.login.matchFulfilled, (state, action) => {
+        state.user = action.payload.appUser;
+        state.isAuthenticated = true;
+      })
+      .addMatcher(userApi.endpoints.login.matchRejected, (_, action) => {
+        console.log("rejected", action);
+      });
+  },
 });
 
-export const { logout } = slice.actions;
-export default slice.reducer;
-
-export const selectIsAuthenticated = (state: RootState) => state.auth.isAuthenticated;
+export const { logout } = authSlice.actions;
+// Expose auth type to main store
+export default authSlice.reducer;
