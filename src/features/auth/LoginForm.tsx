9e931a24a1ec848dcef16eb693b4b1dd945614c9
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-12 22:56:23 +0100

    Add checkbox controller to login form

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,10 +1,11 @@
+import Checkbox from "@/components/checkbox/Checkbox";
 import TextInput from "@/components/text-input/TextInput";
 import { useLoginMutation } from "@/store/services/userApi";
 import { Controller, useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { LoginForm as FormType } from "./AuthFormType";
 import s from "./auth.module.css";
-import { useDispatch } from "react-redux";
 import { rememberMe } from "./slices/authSlice";
 
 const initialValues = {
@@ -62,6 +63,13 @@ const LoginForm = () => {
           <TextInput type="password" label="Password" error={formState.errors?.password?.message} {...props} />
         )}
       />
+
+      <Controller
+        name="rememberMe"
+        control={control}
+        render={({ field }) => <Checkbox {...field} text="Remember me" />}
+      />
+
       <button type="submit" className={s.submitBtn}>
         Sign In
       </button>
