d8fb50a02125743e8d253e21059d7f5734523bfe
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-13 18:47:55 +0100

    Add onclick fn to navigate to details screen

--- src/features/dashboard/RecipeList.tsx
+++ src/features/dashboard/RecipeList.tsx
@@ -1,5 +1,5 @@
 import { Recipe } from "@/store/services/recipeApi";
-import { memo } from "react";
+import { useNavigate } from "react-router-dom";
 import s from "./dashboard.module.css";
 
 interface RecipeListProps {
@@ -7,6 +7,12 @@ interface RecipeListProps {
 }
 
 const RecipeList = ({ list }: RecipeListProps) => {
+  const navigate = useNavigate();
+
+  function onClick(id: string) {
+    navigate("/recipe/" + id);
+  }
+
   return (
     <table className={s.tableList}>
       <thead>
@@ -18,19 +24,19 @@ const RecipeList = ({ list }: RecipeListProps) => {
       </thead>
       <tbody>
         {list.map((item, index) => (
-          <RecipeListItem key={item.id} item={item} index={index} />
+          <RecipeListItem key={item.id} item={item} index={index} onClick={onClick} />
         ))}
       </tbody>
     </table>
   );
 };
 
-const RecipeListItem = memo(({ item, index }: { item: Recipe; index: number }) => (
-  <tr>
+const RecipeListItem = ({ item, index, onClick }: { item: Recipe; index: number; onClick: (id: string) => void }) => (
+  <tr onClick={() => onClick(item.id)}>
     <td>{index}</td>
     <td>{item.title}</td>
     <td>{item.dateCreated}</td>
   </tr>
-));
+);
 
 export default RecipeList;
