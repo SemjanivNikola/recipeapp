f17fc7ba98531bf579bfd5916bc3e30fb40d08d3
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-14 19:52:36 +0100

    Extract list inputs to separate components

--- src/features/create-recipe/CreateRecipeScreen.tsx
+++ src/features/create-recipe/CreateRecipeScreen.tsx
@@ -1,29 +1,27 @@
+import ScreenHeader from "@/components/screen-header/ScreenHeader";
 import ButtonSpinner from "@/components/spinner/ButttonSpinner";
 import TextInput from "@/components/text-input/TextInput";
 import { useCreateRecipeMutation } from "@/store/services/recipeApi";
-import { Controller, useForm } from "react-hook-form";
-import s from "./create-recipe-screen.module.css";
-import ScreenHeader from "@/components/screen-header/ScreenHeader";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { selectUser } from "../auth/slices/authSlice";
+import InstructionsFieldArray from "./InstructionsFieldArray";
+import ListInput from "./ListInput";
+import s from "./create-recipe-screen.module.css";
 import { formatDateForDisplay, formatDateForStore } from "./dateHelper";
 
 type RecipeType = {
   title: string;
   authorId: string;
   dateCreated: Date;
-  _instruction: string;
-  _tag: string;
   instructions: string[];
   tags: string[];
 };
 
 const initialValues = {
   title: "",
-  instructions: [],
-  _instruction: "",
-  tags: [],
-  _tag: "",
+  instructions: ["Type instructions here"],
+  tags: [] as string[],
 };
 
 const DATE = new Date();
@@ -32,7 +30,7 @@ const DATE_FOR_DISPLAY = formatDateForDisplay(DATE);
 const CreateRecipeScreen = () => {
   const user = useSelector(selectUser);
   const [createRecipe, status] = useCreateRecipeMutation();
-  const { handleSubmit, control, formState } = useForm<RecipeType>({
+  const methods = useForm<RecipeType>({
     defaultValues: { authorId: user?.id, dateCreated: DATE, ...initialValues },
     mode: "onBlur",
   });
@@ -50,68 +48,56 @@ const CreateRecipeScreen = () => {
     await createRecipe(formData);
   };
 
+  const tags = methods.getValues("tags");
+
   return (
     <div style={{ height: "100%", backgroundColor: "#ccc" }}>
       <ScreenHeader title="Create new recipe" />
       <div className={s.content}>
-        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
-          <div className={s.row}>
-            <div className={s.w30}>
-              {/* Author and date can not be modified */}
-              <div>Author: {user?.name}</div>
-              <div>Date: {DATE_FOR_DISPLAY}</div>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className={s.form}>
+            <div className={s.row}>
+              <div className={s.w30}>
+                {/* Author and date can not be modified */}
+                <div>Author: {user?.name}</div>
+                <div>Date: {DATE_FOR_DISPLAY}</div>
+              </div>
+              <div className={s.w70}>
+                <Controller
+                  name="title"
+                  control={methods.control}
+                  rules={{
+                    required: true,
+                  }}
+                  render={({ field: props }) => (
+                    <TextInput
+                      type="text"
+                      label="Title"
+                      isFocused
+                      error={methods.formState.errors?.title?.message}
+                      {...props}
+                    />
+                  )}
+                />
+
+                <InstructionsFieldArray />
+
+                <ListInput list={tags} update={(newList) => methods.setValue("tags", newList)} />
+              </div>
             </div>
-            <div className={s.w70}>
-              <Controller
-                name="title"
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({ field: props }) => (
-                  <TextInput type="text" label="Title" isFocused error={formState.errors?.title?.message} {...props} />
-                )}
-              />
 
-              <ul>
-                <li></li>
-              </ul>
-
-              <Controller
-                name="_instruction"
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({ field: props }) => <TextInput type="text" label="Instruction" {...props} />}
-              />
-
-              <ul>
-                <li></li>
-              </ul>
-
-              <Controller
-                name="_tag"
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({ field: props }) => <TextInput type="text" label="Tag" {...props} />}
-              />
+            <div className={s.buttonRow}>
+              {status.isLoading ? (
+                <ButtonSpinner />
+              ) : (
+                <button type="submit" className={s.submitBtn}>
+                  Create
+                </button>
+              )}
+              {status.isLoading ? <ButtonSpinner /> : <button className={s.transparentBtn}>Delete</button>}
             </div>
-          </div>
-
-          <div className={s.buttonRow}>
-            {status.isLoading ? (
-              <ButtonSpinner />
-            ) : (
-              <button type="submit" className={s.submitBtn}>
-                Create
-              </button>
-            )}
-            {status.isLoading ? <ButtonSpinner /> : <button className={s.transparentBtn}>Delete</button>}
-          </div>
-        </form>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
