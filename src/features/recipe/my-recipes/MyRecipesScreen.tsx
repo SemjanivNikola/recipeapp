6f2af5e5a07ffd4926c367fe60444efa085c7c22
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 23:30:26 +0100

    Update style on MyRecipesScreen

--- src/features/recipe/my-recipes/MyRecipesScreen.tsx
+++ src/features/recipe/my-recipes/MyRecipesScreen.tsx
@@ -1,7 +1,6 @@
 import ScreenHeader from "@/components/screen-header/ScreenHeader";
 import Searchbar from "@/components/searchbar/Searchbar";
 import RecipeList from "../../dashboard/RecipeList";
-import s from "../../dashboard/table-list.module.css";
 
 const recipeList = [
   { id: "1", title: "Grilled beef steak and asparagus", dateCreated: "2023-01-01", tags: ["stake", "grill"] },
@@ -9,13 +8,13 @@ const recipeList = [
 
 const MyRecipesScreen = () => {
   return (
-    <div className={s.screen}>
+    <>
       <ScreenHeader title="My recipes" />
-      <div className={s.content}>
+      <div className="p-lg">
         <Searchbar />
         <RecipeList list={recipeList} />
       </div>
-    </div>
+    </>
   );
 };
 
