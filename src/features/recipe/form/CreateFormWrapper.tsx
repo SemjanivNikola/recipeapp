01c4b0fdc8d65424c2e20054eb862faea90aba5a
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 12:34:53 +0100

    Update btn text

--- src/features/recipe/form/CreateFormWrapper.tsx
+++ src/features/recipe/form/CreateFormWrapper.tsx
@@ -77,7 +77,7 @@ const CreateFormWrapper = () => {
             <ButtonSpinner />
           ) : (
             <button type="submit" className={s.submitBtn}>
-              Update
+              Create
             </button>
           )}
         </div>
