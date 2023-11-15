09e743f6ccc68af21d428a4e806df73451af19b3
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 13:31:00 +0100

    Include toast slice in root store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { api } from "./api/api";
+import toast from "@/components/toast/slices/toastSlice";
 import auth from "@/features/auth/slices/authSlice";
 import recipeSlice from "@/features/recipe/slices/recipeSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api/api";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
     recipeSlice,
+    toast,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
