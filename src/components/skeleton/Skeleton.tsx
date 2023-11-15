2f425845046ef6546d5d9df99c25f5065c5db8ac
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 15:49:38 +0100

    Crete skeleton for create-edit screen

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -1,18 +1,73 @@
 import s from "./skeleton.module.css";
 
-const Skeleton = () => {
-  return (
-    <div className={s.skeletonTable}>
-      <div className={s.skeletonTableHeader} />
-      <div className={s.empty} />
-      <div className={s.skeletonTableRow} />
-      <div className={s.empty} />
-      <div className={s.skeletonTableRow} />
-      <div className={s.empty} />
-      <div className={s.skeletonTableRow} />
-      <div className={s.empty} />
+const Skeleton = () => (
+  <div className={s.skeletonScreen}>
+    <div className={s.skeletonScreenHeader}>
+      <div className={s.skeletonFullContent}></div>
     </div>
-  );
-};
+    <div className={s.content}>
+      <div className={s.skeletonForm}>
+        <div className={s.row}>
+          <div className={s.w30}>
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+          </div>
+          <div className={s.w70}>
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+          </div>
+        </div>
+        <div className={s.skeletonActionRow}>
+          <div className={s.skeletonBtn} />
+          <div className={s.skeletonBtn} />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const CreateEditSkeleton = () => (
+  <div className={s.skeletonScreen}>
+    <div className={s.skeletonScreenHeader}>
+      <div className={s.skeletonFullContent}></div>
+    </div>
+    <div className={s.content}>
+      <div className={s.skeletonForm}>
+        <div className={s.row}>
+          <div className={s.w30}>
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+          </div>
+          <div className={s.w70}>
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+            <div className={s.skeletonContent} />
+          </div>
+        </div>
+        <div className={s.skeletonActionRow}>
+          <div className={s.skeletonBtn} />
+          <div className={s.skeletonBtn} />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const ListSkeleton = () => (
+  <div className={s.skeletonTable}>
+    <div className={s.skeletonTableHeader} />
+    <div className={s.empty} />
+    <div className={s.skeletonTableRow} />
+    <div className={s.empty} />
+    <div className={s.skeletonTableRow} />
+    <div className={s.empty} />
+    <div className={s.skeletonTableRow} />
+    <div className={s.empty} />
+  </div>
+);
+
+Skeleton.List = ListSkeleton;
+Skeleton.CreateEdit = CreateEditSkeleton;
 
 export default Skeleton;
