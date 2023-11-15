5241a923851479058bcd0bd1ac0531de8743c67e
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 12:34:14 +0100

    Update recipeIds prop in store on action

--- src/features/recipe/slices/recipeSlice.ts
+++ src/features/recipe/slices/recipeSlice.ts
@@ -1,4 +1,4 @@
-import { Recipe } from "@/store/services/recipeApi";
+import { Recipe, recipeApi } from "@/store/services/recipeApi";
 import { userApi } from "@/store/services/userApi";
 import { RootState } from "@/store/store";
 import { createSlice } from "@reduxjs/toolkit";
@@ -14,7 +14,9 @@ function getInitialState(): RecipeSlice {
   const recipeIds: string[] | null = recipeStorage ? JSON.parse(recipeStorage) : null;
   return { recipes: [], recipeIds: recipeIds ?? [] };
 }
-
+/**
+ * @note From **recipeIds** prop here we check if user is recipe owner
+ */
 const recipeSlice = createSlice({
   name: "recipes",
   initialState: getInitialState(),
@@ -22,6 +24,20 @@ const recipeSlice = createSlice({
   extraReducers: (builder) => {
     builder.addMatcher(userApi.endpoints.login.matchFulfilled, (state, action) => {
       state.recipeIds = action.payload.appUser.recipeIds;
+      storeRecipeIds(state.recipeIds);
+    });
+    builder.addMatcher(recipeApi.endpoints.createRecipe.matchFulfilled, (state, action) => {
+      state.recipeIds.push(action.payload.recipeId);
+
+      // Update local storage so it's up to date with changes
+      storeRecipeIds(state.recipeIds);
+    });
+    builder.addMatcher(recipeApi.endpoints.deleteRecipe.matchFulfilled, (state, action) => {
+      const idToDel = action.meta.arg.originalArgs;
+      const updatedList = state.recipeIds.filter((item) => item !== idToDel);
+      state.recipeIds = updatedList;
+
+      // Update local storage so it's up to date with changes
       storeRecipeIds(state.recipeIds);
     });
   },
