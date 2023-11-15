0da990581dcd2881a9500102dc38aca5bf0cfce2
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 15:12:45 +0100

    Remove redundant div

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -4,15 +4,13 @@ const Skeleton = () => {
   return (
     <div className={s.skeletonTable}>
       <div className={s.skeletonTableHeader} />
-      <div className={s.skeletonTableBody}>
-        <div className={s.empty} />
-        <div className={s.skeletonTableRow} />
-        <div className={s.empty} />
-        <div className={s.skeletonTableRow} />
-        <div className={s.empty} />
-        <div className={s.skeletonTableRow} />
-        <div className={s.empty} />
-      </div>
+      <div className={s.empty} />
+      <div className={s.skeletonTableRow} />
+      <div className={s.empty} />
+      <div className={s.skeletonTableRow} />
+      <div className={s.empty} />
+      <div className={s.skeletonTableRow} />
+      <div className={s.empty} />
     </div>
   );
 };
