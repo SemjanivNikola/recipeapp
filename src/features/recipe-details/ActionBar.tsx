9769cf90c8c31ed701ec4fc3c45dd6467267e2f6
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-15 11:07:05 +0100

    Update link to navigate to edit screen

--- src/features/recipe-details/ActionBar.tsx
+++ src/features/recipe-details/ActionBar.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import s from "./recipe-details-screen.module.css";
 import { isRecipeOwner } from "./recipeUtils";
 import { selectRecipeIds } from "./slices/recipeSlice";
+import { Link } from "react-router-dom";
 
 const ActionBar = ({ recipeId }: { recipeId: string }) => {
   const recipeIds = useSelector(selectRecipeIds);
@@ -10,7 +11,9 @@ const ActionBar = ({ recipeId }: { recipeId: string }) => {
   if (isOwner)
     return (
       <div className={s.actionBar}>
-        <button className={s.btnTransparent}>Edit</button>
+        <button className={s.btnTransparent}>
+          <Link to={`/edit-recipe/${recipeId}`}>Edit</Link>
+        </button>
         <button className={s.btnDanger}>Delete</button>
       </div>
     );
