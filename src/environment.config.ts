bd942dd7bc53a327e66860a933a9950af0179407
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-16 12:45:16 +0100

    Add env variable

--- src/environment.config.ts
+++ src/environment.config.ts
@@ -2,6 +2,7 @@ export default {
   AUTH_ID: import.meta.env.VITE_AUTH_ID,
   LOGIN_URL: import.meta.env.VITE_LOGIN_URL,
   REGISTER_URL: import.meta.env.VITE_REGISTER_URL,
+  GET_USER_URL: import.meta.env.VITE_GET_USER_URL,
   GET_ALL_RECIPES_URL: import.meta.env.VITE_GET_ALL_RECIPES_URL,
   GET_RECIPE_URL: import.meta.env.VITE_GET_RECIPE_URL,
   ADD_RECIPE_URL: import.meta.env.VITE_ADD_RECIPE_URL,
