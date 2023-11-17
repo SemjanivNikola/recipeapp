d1234a5f7720a40b8d77cdb0aed40a952c288c45
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-17 18:05:05 +0100

    Update component file to use router + redux

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -14,26 +14,28 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import { mount } from 'cypress/react18'
+import { mount } from "cypress/react18";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../../src/store/store";
 
-// Augment the Cypress namespace to include type definitions for
-// your custom command.
-// Alternatively, can be defined in cypress/support/component.d.ts
-// with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount
-    }
-  }
-}
+Cypress.Commands.add("mount", (component, options = {}) => {
+  const { routerProps, reduxStore = store, ...mountOptions } = options;
 
-Cypress.Commands.add('mount', mount)
+  const wrapped = (
+    <Provider store={reduxStore}>
+      <BrowserRouter {...routerProps}>{component}</BrowserRouter>
+    </Provider>
+  );
+
+  return mount(wrapped, mountOptions);
+});
 
 // Example use:
 // cy.mount(<MyComponent />)
