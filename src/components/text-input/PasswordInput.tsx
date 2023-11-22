5df3bfe5446301e32d34aacb4d910e152b904799
SemjanivNikola <nikola.sem450@gmail.com>
2023-11-22 13:56:38 +0100

    Add toggle fn to set input type

--- src/components/text-input/PasswordInput.tsx
+++ src/components/text-input/PasswordInput.tsx
@@ -1,17 +1,36 @@
-import { forwardRef } from "react";
+import { forwardRef, useCallback, useState } from "react";
 import Icon from "../icons/Icon";
 import SimpleInput from "./SimpleInput";
 import { PasswordInputProps } from "./TextInputProps";
 import s from "./text-input.module.css";
 
+enum InputType {
+  TEXT = "text",
+  PASSWORD = "password",
+}
+
 const PasswordInput = forwardRef<HTMLInputElement | null, PasswordInputProps>(function PasswordInput(
   { error, ...otherProps },
   ref
 ) {
+  const [shouldShowPass, setShouldShowPass] = useState(false);
+
+  const PassIcon = useCallback(() => {
+    return shouldShowPass ? <Icon name="eye-outline" /> : <Icon name="eye-closed" />;
+  }, [shouldShowPass]);
+
+  const type = shouldShowPass ? InputType.TEXT : InputType.PASSWORD;
+
+  function onClick() {
+    setShouldShowPass(!shouldShowPass);
+  }
+
   return (
     <div className="relative mb-m text-start">
-      <SimpleInput {...otherProps} ref={ref}>
-        <Icon name="plus" />
+      <SimpleInput type={type} {...otherProps} ref={ref}>
+        <button type="button" onClick={onClick} className={s.togglePassTypeBtn}>
+          <PassIcon />
+        </button>
       </SimpleInput>
       <p className={s.inputError}>{error}</p>
     </div>
